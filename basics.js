256c9675da95f745f66a837356261b68a823f956
Andrea Valgeirsdottir <andr60c0@stud.kea.dk>
2020-09-01 12:53:27 +0200

    basics done

--- basics.js
+++ basics.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+"use strict";
+
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+const name = "Peter-Marius";
+console.log(name);
+console.log(/-/.test(name));
+
+const text = "Andrea";
